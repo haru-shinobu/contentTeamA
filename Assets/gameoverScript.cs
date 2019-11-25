5f3d27216e701b200aebf4d825b9b40feeeb7d2f
haru-shinobu <r.t0desu@gmail.com>
2019-11-25 16:00:24 +0900

    GameOver2 TEST

---
+++ Assets/gameoverScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameoverScript : MonoBehaviour
+{
+
+    RectTransform rec;
+    Vector3 scale;
+    float com;
+    void Start()
+    {
+        com = 0;
+        rec = gameObject.GetComponent<RectTransform>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scale = new Vector3(1, com-0.2f, 1);
+        rec.localScale = scale;
+        Debug.Log(rec.localScale);
+
+
+        if (com < 3)
+            com += Time.deltaTime*0.2f;
+    }
+}
