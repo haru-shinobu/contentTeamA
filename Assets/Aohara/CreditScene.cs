cab3d3c15ef7752a536a56cf37e506d6fd23cee9
masayuki5640@gmail.com <masayuki5640@gmail.com>
2019-12-10 18:56:02 +0900

    Title

---
+++ Assets/Aohara/CreditScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditScene : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameCredit");//シーン名が入る
+    }
+
+
+}
---
+++ Assets/Aohara/Stage3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage3 : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Stage3");//シーン名が入る
+    }
+
+
+}
+
