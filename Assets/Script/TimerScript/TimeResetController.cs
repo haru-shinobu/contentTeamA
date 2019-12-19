8ef29c190dcf40024fc490edb7f334f4dcfa54dd
haru-shinobu <r.t0desu@gmail.com>
2019-12-19 15:34:11 +0900

    タイトル戻ったときの音修正

--- Assets/Script/TimerScript/TimeResetController.cs
+++ Assets/Script/TimerScript/TimeResetController.cs
@@ -7,6 +7,8 @@ public class TimeResetController : MonoBehaviour
 
     void Start()
     {
+        AudioSource aus = GameObject.Find("SoundLoop").GetComponent<AudioSource>();
+        aus.Play();
         //カーソル表示・カーソルを画面内にロック
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
